6b9b8463d1b0b54cb1ff1400e150a76a067ad15a
paleto30 <paleto0817@gmail.com>
2023-06-24 18:24:39 -0500

    feat (functions): :rocket:  se arreglaron y crearon nuevas funciones
    se arreglaron las funciones de get  y set del modelo
    
    se arreglo tambien la funcion Find, para que retorne una instancia del modelo, en vez de un array
    
    se crearon las funciones save , update, y delete del modelo
    para usarlas cuando sea necesario.

--- backend/app/Models/Areas/Areas.php
+++ backend/app/Models/Areas/Areas.php
@@ -9,25 +9,37 @@ use PDO;
 class  Areas
 {
 
-    public function __construct(private $name_area)
+    public function __construct(private $name_area, private $id = null)
     {
         $this->name_area = $name_area;
+        $this->id = $id;
     }
 
     public function __set($name, $value)
     {
         if (property_exists($this, $name)) {
                 $this->$name = $value;
+        }else{
+            throw new Exception("Propiedad invalida: " . $name);
         }
-        throw new Exception("Propiedad invalida: " . $name);
     }
 
     public function __get($name)
     {
         if (property_exists($this, $name)) {
             return $this->$name;
+        }else{
+            throw new Exception("Propiedad invalida: " . $name);
         }
-        throw new Exception("Propiedad invalida: " . $name);
+    }
+
+
+    public function toString()
+    {
+        return [
+            'id' => $this->id,
+            'name_area' => $this->name_area, 
+        ];
     }
 
 
@@ -60,15 +72,73 @@ class  Areas
             $query = "SELECT * from areas WHERE id = ?";
             $stament = $db->connect()->prepare($query);
             $stament->execute([$id]);
-            $area = $stament->fetch(PDO::FETCH_ASSOC);  
-            return $area;
+            $result =  $stament->fetch(PDO::FETCH_ASSOC);  
+            $db->closed();
+
+            $area = new self($result['name_area'],$result['id']);
+            
+            if ($area->id !== null) {
+                return $area;
+            }
+            return null;
+
         } catch (\Exception $e) {
-            return  $e;
+           
         }
-    }  
+    } 
 
 
 
+
+    //insertar una nueva Area
+    public function save(){
+        try {
+            $db = new Conexion;
+
+            $query = "INSERT INTO areas (name_area) VALUES ( ? )";
+            $stament  = $db->connect()->prepare($query);
+            $stament->execute([$this->name_area]);
+            
+            return true;
+
+        } catch (\Exception $e) {
+            return  $e->getMessage();
+        }
+    }
+
+
+    // funcion para actualizar un registro de areas
+    public function update(){
+        try {
+            $db = new Conexion;
+
+            $query = "UPDATE areas SET name_area = ? WHERE id = ?";
+            $stament = $db->connect()->prepare($query);
+            $stament->execute([$this->name_area,$this->id]);
+            $db->closed();
+            return true;
+            
+        } catch (\Throwable $th) {
+            return  $th->getMessage();
+        }
+    }
+
+
+
+    // funcion para eliminar un registro si existe
+    public function delete(){   
+        try {
+            $db = new Conexion;
+
+            $query = "DELETE FROM areas WHERE id = ?";
+            $stament = $db->connect()->prepare($query);
+            $stament->execute([$this->id]);
+            $db->closed();
+            return true;
+        } catch (\Throwable $th) {
+             return  $th->getMessage();
+        }
+    }
     
 
 
