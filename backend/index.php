fd6c1505d70c6e9554dd7e937c7fee1866433462
paleto30 <paleto0817@gmail.com>
2023-06-24 18:21:02 -0500

    fix (headers): :rocket: header para application/json

--- backend/index.php
+++ backend/index.php
@@ -3,9 +3,11 @@
 // *  Configurar encabezados CORS  *
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // ->  Este encabezado especifica los métodos HTTP permitidos para las solicitudes. En este caso, se permiten los métodos GET, POST y OPTIONS. Puedes ajustar esta lista según tus necesidades.
+header("Content-Type: application/json");
+//header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // ->  Este encabezado especifica los métodos HTTP permitidos para las solicitudes. En este caso, se permiten los métodos GET, POST y OPTIONS. Puedes ajustar esta lista según tus necesidades.
 // header("Access-Control-Allow-Headers: Origin, Content-Type, X-Requested-With, Accept"); -> : Este encabezado especifica los encabezados personalizados permitidos en las solicitudes. Aquí se incluyen algunos ejemplos comunes como Origin, Content-Type, X-Requested-With y Accept. Puedes agregar o quitar encabezados según tus requisitos.
 require_once './vendor/autoload.php';
 require_once './routes/api.php';
+
 
 ?>
