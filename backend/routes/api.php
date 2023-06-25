896e8ec32a6e1150675b87aa51633db43eba9e05
paleto30 <paleto0817@gmail.com>
2023-06-25 10:50:53 -0500

    feat (rutas) : :rocket:  se crearon las rutas de la tabla position

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,13 +4,47 @@ namespace Routes;
 
 use Dotenv\Dotenv;
 use Bramus\Router\Router;
-use Config\Database\Conexion;
-use PDO;
+
 
 // Router
 $router = new Router();
 $dotenv = Dotenv::createImmutable('./config/env/');
 $dotenv->load();
+
+
+
+// rutas para areas
+$router->mount('/api/areas', function() use($router){
+    // ruta para obtener todas las areas
+    $router->get('/','App\Controllers\Areas\AreasController@getAllAreas');
+    // ruta para crear una categoria 
+    $router->post('/','App\Controllers\Areas\AreasController@insertArea');
+    // ruta para actualizar una Area
+    $router->post('/{id}','App\Controllers\Areas\AreasController@updateArea');
+    // ruta para eliminar un Area
+    $router->delete('/{id}','App\Controllers\Areas\AreasController@deleteArea');
+}); 
+
+
+
+// rutas para position
+$router->mount('/api/position',function() use($router){
+    // ruta  para obtener todas las positions
+    $router->get('/', 'App\Controllers\Position\PositionController@getAllPositions');
+    // ruta para agregar un registro en tabla position 
+    $router->post('/', 'App\Controllers\Position\PositionController@insertPosition');
+    // ruta para actualizar un registro de la tabla  position
+    $router->post('/{id}','App\Controllers\Position\PositionController@updatePositon');
+    //ruta para eliminar un registro de la tabla position
+    $router->delete('/{id}','App\Controllers\Position\PositionController@deletePosition');
+});
+
+
+
+
+
+
+
 
 
 
